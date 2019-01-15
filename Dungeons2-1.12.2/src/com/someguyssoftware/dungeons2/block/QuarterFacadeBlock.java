3585da14df48a330c9a9002dc07df3c45882667e
gottsch <17928819+gottsch@users.noreply.github.com>
2019-01-15 09:08:29 -0500

    fix quarter facade block models and bounding boxes

--- Dungeons2-1.12.2/src/com/someguyssoftware/dungeons2/block/QuarterFacadeBlock.java
+++ Dungeons2-1.12.2/src/com/someguyssoftware/dungeons2/block/QuarterFacadeBlock.java
@@ -3,6 +3,7 @@
  */
 package com.someguyssoftware.dungeons2.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.math.AxisAlignedBB;
 
@@ -39,17 +40,24 @@ public class QuarterFacadeBlock extends BasicFacadeBlock {
 				new AxisAlignedBB(0F, 0F, 0F, 1F, 1F, 1F),	// E
 				
 				// outer left
-				new AxisAlignedBB(0.5F, 0F, 0F, 1F, 1F, 0.5F),	// S
-				new AxisAlignedBB(0.5F, 0F, 0.5F, 1F, 1F, 1F),	// W
-				new AxisAlignedBB(0F, 0F, 0.5F, 0.5F, 1F, 1F),	// N (starts at half)
-				new AxisAlignedBB(0F, 0F, 0F, 0.5F, 1F, 0.5F),	// E (starts at zero)		
+				new AxisAlignedBB(0.75F, 0F, 0F, 1F, 1F, 0.25F),	// S
+				new AxisAlignedBB(0.75F, 0F, 0.75F, 1F, 1F, 1F),	// W
+				new AxisAlignedBB(0F, 0F, 0.75F, 0.25F, 1F, 1F),	// N (starts at half)
+				new AxisAlignedBB(0F, 0F, 0F, 0.75F, 1F, 0.75F),	// E (starts at zero)		
 				
 				// outer right
-				new AxisAlignedBB(0F, 0F, 0F, 0.5F, 1F, 0.5F),	// S
-				new AxisAlignedBB(0.5F, 0F, 0F, 1F, 1F, 0.5F),	// W
-				new AxisAlignedBB(0.5F, 0F, 0.5F, 1F, 1F, 1F),	// N (starts at half)
-				new AxisAlignedBB(0F, 0F, 0.5F, 0.5F, 1F, 1F)	// E (starts at zero)					
+				new AxisAlignedBB(0F, 0F, 0F, 0.25F, 1F, 0.25F),	// S
+				new AxisAlignedBB(0.75F, 0F, 0F, 1F, 1F, 0.25F),	// W
+				new AxisAlignedBB(0.75F, 0F, 0.75F, 1F, 1F, 1F),	// N (starts at half)
+				new AxisAlignedBB(0F, 0F, 0.75F, 0.25F, 1F, 1F)	// E (starts at zero)					
 		});
 	}
+	
+    /**
+     * Checks if a block is basic
+     */
+    public static boolean isBlockBasic(Block block) {
+        return block instanceof QuarterFacadeBlock;
+    }
 
 }
